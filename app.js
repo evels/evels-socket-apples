8a90af085b54b94a10e9cbaa47053c31ba93ffa2
evels <eveleensung@gmail.com>
2014-12-10 20:58:18 -0500

    redCards and greenCards succesfully sent

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var usernames = {};
 
 io.sockets.on('connection', function (socket) {
 
-    
+
     // when the client emits 'sendchat', this listens and executes
     //socket.on('sendchat', function (data) {
     // we tell the client to execute 'updatechat' with 2 parameters
@@ -31,33 +31,32 @@ io.sockets.on('connection', function (socket) {
 
     // when the client emits 'adduser', this listens and executes
     socket.on('adduser', function(username){
-        console.log('add user '+ username);
-        // we store the username in the socket session for this client
-        socket.username = username;
-        // add the client's username to the global list
-        usernames[username] = username;
-        // echo to client they've connected
-        socket.emit('updatechat', 'SERVER', 'you have connected');
-        // echo globally (all clients) that a person has connected
-        socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
-        // update the list of users in chat, client-side
-        io.sockets.emit('updateusers', usernames);
-        io.sockets.emit('dealcards', username);
- 
+	console.log('add user '+ username);
+	// we store the username in the socket session for this client
+	socket.username = username;
+	// add the client's username to the global list
+	usernames[username] = username;
+	// echo to client they've connected
+	socket.emit('updatechat', 'SERVER', 'you have connected');
+	// echo globally (all clients) that a person has connected
+	socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+	// update the list of users in chat, client-side
+	io.sockets.emit('updateusers', usernames);
+
     });
 
     // when the user disconnects.. perform this
     socket.on('disconnect', function(){
-        // remove the username from global usernames list
-        delete usernames[socket.username];
-        // update list of users in chat, client-side
-        io.sockets.emit('updateusers', usernames);
-        // echo globally that this client has left
-        socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+	// remove the username from global usernames list
+	delete usernames[socket.username];
+	// update list of users in chat, client-side
+	io.sockets.emit('updateusers', usernames);
+	// echo globally that this client has left
+	socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
     });
-    
-    socket.on('begingame', function() {
-        console.log('begin game');
-       io.sockets.emit('startgame'); 
+
+    socket.on('updatecarddata', function(red, green) {
+	io.sockets.emit('updatecarddata', red, green);
     });
+
 });
--- js/script.js
+++ js/script.js
@@ -1,72 +1,104 @@
-var redCards = ["Morgan Freeman", "whatwhat"];
-var greenCards = ["sad", "quiet"];
+var redCards = [];
+var greenCards = [];
 var usernames = {};
 
 
-$(document).ready(function() {
 
-    //load cards
-    // $.get('languages.txt', function(txtFile){
-    //    var languages = txtFile.split("\n");
-    //    $("#tags").autocomplete({
-    //        source: languages
-    //    });
-    //});
+$(document).ready(function() {
 
     var socket = io.connect('http://localhost:8080');
 
     //getting player name
     $('.player-enter').submit(function(event) {
-        event.preventDefault();
-        var name = $('#player-name').val();
-        socket.emit('adduser', name);
-        $(this).fadeOut();
-        return;
+	event.preventDefault();
+	var name = $('#player-name').val();
+	socket.emit('adduser', name);
+	$(this).fadeOut();
+	return;
     });
-    
+
     $('.game-begin').on('click', function() {
-        $('.game-enter .error').text();
-       if(Object.keys(usernames).length > 2) {
-           socket.emit('begingame');
-       } 
-       else {
-           $('.game-enter .error').text("You must have more players!");
-       }
+
+	//check for enough players
+	if(Object.keys(usernames).length < 2) {
+	    $('.game-enter .error').text("You must have more players!");
+	}
+	else {
+	    $.when(
+		// load cards
+		$.get('redcards.txt', function(txtFile){
+		    var temp_array = txtFile.split("\n");
+		    for(var l = 0; l < temp_array.length; l++) {
+			redCards.push({
+			    key:temp_array[l],
+			    used:false
+			});
+		    }
+		}),
+		$.get('greencards.txt', function(txtFile){
+		    var temp_array = txtFile.split("\n");
+		    for(var l = 0; l < temp_array.length; l++) {
+			greenCards.push({
+			    key:temp_array[l],
+			    used:false
+			});
+		    }
+		})
+
+		).then(function() {
+		socket.emit('updatecarddata', redCards, greenCards);
+		
+
+	    });
+	}
     });
+
 
     // on connection to server, ask for user's name with an anonymous callback
     socket.on('connect', function(){
-    
-        });
+
+	});
 
 
     // listener, whenever the server emits 'updatechat', this updates the chat body
     socket.on('updatechat', function (username, data) {
-        $('#conversation').append('<b>'+username + ':</b> ' + data + '<br>');
+	$('#conversation').append('<b>'+username + ':</b> ' + data + '<br>');
     });
 
     // listener, whenever the server emits 'updateusers', this updates the username list
     socket.on('updateusers', function(data) {
-        usernames = data;    
-        $('#users').empty();
-        $.each(data, function(key, value) {
-            $('#users').append('<div>' + key + '</div>');
-        });
+	usernames = data;
+	$('#users').empty();
+	$.each(data, function(key, value) {
+	    $('#users').append('<div>' + key + '</div>');
+	});
     });
 
-    socket.on('dealcards', function(newplayer) {
-        $('.player').text(redCards[0]); 
-    });
-    
-    socket.on('startgame', function() {
-        $('.game-enter').fadeOut();
-        $('.dealer').text(greenCards[0]);
+    socket.on('updatecarddata', function(red, green) {
+	redCards = red;
+	greenCards = green;
     });
 
-    // on load of page
-    $(function(){
-    
-        });
 
 });
- 
+// on load of page
+$(function(){
+
+    });
+
+
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getCard(cards) {
+    var random = getRandomInt(0, cards.length);
+    if (cards[random].used == true) {
+	getCard(cards);
+    }
+    console.log(cards[random]);
+    cards[random].used = true;
+    console.log(cards[random]);
+    return cards[random].key;
+}
